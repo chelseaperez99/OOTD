d93cec7912e578b52a2ec1bc9408a13363c577e3
cmperez <cmperez@gips.org>
2016-11-04 13:38:44 -0500

    More modified stuff

--- app/src/main/java/com/example/cmperez/ootd/MainActivity.java
+++ app/src/main/java/com/example/cmperez/ootd/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.cmperez.ootd;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String userSpace;
+    private static String passSpace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,56 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button button1 = (Button) findViewById(R.id.login);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent k = new Intent(MainActivity.this, Homescreen.class);
-                startActivity(k);
+                Intent j = new Intent(MainActivity.this, Homescreen.class);
+                startActivity(j);
             }
         });
+        final EditText userspace = (EditText)findViewById(R.id.userspace);
+        userspace.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                userSpace = userspace.getText().toString();
+            }
+        });
+        final EditText passspace = (EditText)findViewById(R.id.passspace);
+        passspace.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                passSpace = passspace.getText().toString();
+            }
+        });
+    }
+    private void savedata () {
+        SharedPreferences sharedPreferences = getSharedPreferences("Account", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("userspace", userSpace);
+        editor.putString("passspace", passSpace);
+        editor.apply();
     }
 }
