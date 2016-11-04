b8c8a6b6f559acccf687a0e0dc7d2dbc21cdd9b3
cmperez <cmperez@gips.org>
2016-11-04 14:32:05 -0500

    More modified stuff

--- app/src/main/java/com/example/cmperez/ootd/PersonalStyle.java
+++ app/src/main/java/com/example/cmperez/ootd/PersonalStyle.java
@@ -8,50 +8,56 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-public class PersonalStyle extends AppCompatActivity
-{
+public class PersonalStyle extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personalstyle);
 
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PersonalStyle.this, Homescreen.class);
+                startActivity(i);
 
-        CheckBox casual = (CheckBox) findViewById(R.id.casual);
-        casual.setOnClickListener(onClickListener);
+                CheckBox casual = (CheckBox) findViewById(R.id.casual);
+                casual.setOnClickListener(onClickListener);
 
-        CheckBox comfy = (CheckBox)findViewById(R.id.comfylazy);
-        comfy.setOnClickListener(onClickListener);
+                CheckBox comfy = (CheckBox) findViewById(R.id.comfylazy);
+                comfy.setOnClickListener(onClickListener);
 
-        CheckBox professional = (CheckBox)findViewById(R.id.professional);
-        professional.setOnClickListener(onClickListener);
+                CheckBox professional = (CheckBox) findViewById(R.id.professional);
+                professional.setOnClickListener(onClickListener);
 
-        CheckBox elegant = (CheckBox)findViewById(R.id.elegantclassy);
-        elegant.setOnClickListener(onClickListener);
+                CheckBox elegant = (CheckBox) findViewById(R.id.elegantclassy);
+                elegant.setOnClickListener(onClickListener);
 
 
-
-}
-    public View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.casual:
-                    CheckBox casual = (CheckBox)findViewById(R.id.casual);
-                    break;
-                case R.id.comfylazy:
-                    CheckBox comfy = (CheckBox)findViewById(R.id.comfylazy);
-                    break;
-                case R.id.professional:
-                    CheckBox professional = (CheckBox)findViewById(R.id.professional);
-                    break;
-                case R.id.elegantclassy:
-                    CheckBox elegant = (CheckBox)findViewById(R.id.elegantclassy);
-                    break;
             }
-        }
-    };
 
+            public View.OnClickListener onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.casual:
+                            CheckBox casual = (CheckBox) findViewById(R.id.casual);
+                            break;
+                        case R.id.comfylazy:
+                            CheckBox comfy = (CheckBox) findViewById(R.id.comfylazy);
+                            break;
+                        case R.id.professional:
+                            CheckBox professional = (CheckBox) findViewById(R.id.professional);
+                            break;
+                        case R.id.elegantclassy:
+                            CheckBox elegant = (CheckBox) findViewById(R.id.elegantclassy);
+                            break;
+                    }
+                }
+            };
+
+        });
     }
-
+}
